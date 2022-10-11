7fc9e783e567c81044ce3c3dac399e0df1e6ccff
Mohammad Ali <itsproali@gmail.com>
2022-10-11 20:21:01 +0600

    ✅ Feature Section Added

--- JS/slder.js
+++ JS/slder.js
@@ -5,22 +5,26 @@ const carouselImages = document.getElementById("carousel-images");
 
 const images = ["assets/Page1.jpg", "assets/Page2.jpg", "assets/Page3.jpg"];
 
+// Default Image
 let imageIndex = 0;
-
 carouselImages.innerHTML = `
 <div class="image-wrapper">
     <img src=${images[imageIndex]} alt="carousel-image">
 </div>
 `;
 
-// Display First Image
-btn1.addEventListener("click", () => {
-  imageIndex = 0;
+// Change image using button
+const changeImageWithButton = (index) => {
   carouselImages.innerHTML = `
     <div class="image-wrapper">
-        <img src=${images[imageIndex]} alt="carousel-image">
+        <img src=${images[index]} alt="carousel-image">
     </div>
     `;
+};
+
+// Display First Image
+btn1.addEventListener("click", () => {
+  changeImageWithButton(0);
   btn1.classList.add("active-button");
   btn2.classList.remove("active-button");
   btn3.classList.remove("active-button");
@@ -28,12 +32,7 @@ btn1.addEventListener("click", () => {
 
 // Display Second Image
 btn2.addEventListener("click", () => {
-  imageIndex = 1;
-  carouselImages.innerHTML = `
-    <div class="image-wrapper">
-        <img src=${images[imageIndex]} alt="carousel-image">
-    </div>
-    `;
+  changeImageWithButton(1);
   btn2.classList.add("active-button");
   btn1.classList.remove("active-button");
   btn3.classList.remove("active-button");
@@ -41,12 +40,7 @@ btn2.addEventListener("click", () => {
 
 // Display Third Image
 btn3.addEventListener("click", () => {
-  imageIndex = 2;
-  carouselImages.innerHTML = `
-    <div class="image-wrapper">
-        <img src=${images[imageIndex]} alt="carousel-image">
-    </div>
-    `;
+  changeImageWithButton(2);
   btn3.classList.add("active-button");
   btn1.classList.remove("active-button");
   btn2.classList.remove("active-button");
